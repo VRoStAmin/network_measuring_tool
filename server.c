3719724aed4d25f10e5dd80d4f1875a332f52942
VRoStAmin <manosmastorakis579@gmail.com>
2026-03-25 23:55:28 +0200

    Too late to continue this.

--- server.c
+++ server.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "server.h"
 
 int run_server(configuration_flags_t *cft) {
+    int server_sock; /* One port where the server will be listening on. */ 
+    int client_sock; /* One port where the communication between a specific client and the server will happen. */
+
     printf("Server mode selected...\n");
     return 0;
 }
